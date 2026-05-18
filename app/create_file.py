1ed0553d0bafee5f75d1838c2f3e75bb6bc8a7ef
Andre Marcio <andre.marcio.ti@gmail.com>
2026-05-18 16:45:14 -0300

    verify task

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,21 @@
-# write your code here
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', nargs='+', help='directory parts')
+parser.add_argument('-f', help='file name')
+args = parser.parse_args()
+
+
+if args.d:
+    path = os.path.join(*args.d)
+    os.makedirs(path, exist_ok=True)
+
+
+lines = []
+while True:
+    line = input("Enter content line: ")
+    if line == "stop":
+        break
+    lines.append(line)
